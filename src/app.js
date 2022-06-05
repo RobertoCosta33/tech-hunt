50e78e0182924eb5797234b0eee6fd329da227b7
Roberto Barbosa Costa <romeromatallica@gmail.com>
2022-06-05 18:26:09 -0300

    refactor project istructure

--- src/app.js
+++ src/app.js
@@ -1,60 +1,7 @@
-import { game } from "./pages/game.js";
+import { login } from "./pages/login.js";
 
 export const app = () => {
-  const app = document.getElementById("root");
-  const loginTitle = document.createElement("h1");
-  const title = document.createElement("h1");
-  const container = document.createElement("div");
-  const labelName = document.createElement("label");
-  const inputName = document.createElement("input");
-  const labelPassword = document.createElement("label");
-  const inputPassword = document.createElement("input");
-  const loginButton = document.createElement("button");
-  const spanPassword = document.createElement("span");
-
-  loginTitle.innerText = "Página de login";
-  loginButton.innerText = "Sing in";
-  labelName.innerText = "*Nome:";
-  labelPassword.innerText = "*Senha:";
-
-  inputName.placeholder = "Fulano Sicrano";
-  inputPassword.placeholder = "Fulano123#";
-
-  app.classList.remove("root");
-
-  app.classList.add("rootLogin");
-  title.classList.add("title");
-  container.classList.add("container");
-  loginButton.classList.add("loginButton");
-  labelName.classList.add("loginLabels");
-  labelPassword.classList.add("loginLabels");
-  inputName.classList.add("inputsLogin");
-  inputPassword.classList.add("inputsLogin");
-  spanPassword.classList.add("span");
-
-  app.appendChild(loginTitle);
-  app.appendChild(container);
-  container.appendChild(labelName);
-  container.appendChild(inputName);
-  container.appendChild(labelPassword);
-  container.appendChild(inputPassword);
-  container.appendChild(spanPassword);
-  container.appendChild(loginButton);
-
-  loginButton.addEventListener("click", () => {
-    if (inputName.value != "" && inputPassword.value != "") {
-      app.classList.add("root");
-      app.removeChild(loginTitle);
-      app.removeChild(container);
-
-      app.appendChild(title);
-
-      title.innerText = `Bem Vindo ${inputName.value}`;
-      game();
-    } else {
-      spanPassword.innerText = "O nome de usuário e senha são obrigatório";
-    }
-  });
+  login();
 };
 
 app();
